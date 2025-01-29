a4b02c39acecde23b6e0ce6c8d46b0290333d5ef
Shreyajagda <167760364+Shreyajagda@users.noreply.github.com>
2025-01-29 17:56:36 +0530

    Add files via upload

---
+++ Project-3/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1/project-1')
+
+const db = mongoose.connection;
+
+db.once("open",(err)=>{
+    err ? console.log() : console.log("database connected");
+    
+    
+})
+
+module.exports = db ;
